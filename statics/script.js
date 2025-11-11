25e79124e06486a3bfe141ee64b40a7b30f3c564
navjotcour04 <navjotcour147@gmail.com>
2025-11-11 14:59:37 +0530

    Admin Page

---
+++ statics/script.js
@@ -0,0 +1,11 @@
+// Dashboard Data Update Example
+const total = 1200;
+const present = 950;
+const absent = total - present;
+const percent = ((present / total) * 100).toFixed(2);
+
+// Update text dynamically
+document.getElementById("totalStudents").textContent = total;
+document.getElementById("presentStudents").textContent = present;
+document.getElementById("absentStudents").textContent = absent;
+document.getElementById("attendancePercent").textContent = percent + "%";
